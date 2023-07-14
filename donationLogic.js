790e91253a815773739a34b65563324d5ac2b4e1
atat072 <paulhenke46@gmail.com>
2023-07-14 11:00:00 +0200

    added donation Overview

--- donationLogic.js
+++ donationLogic.js
@@ -1,21 +1,3 @@
-//form validtion
-(() => {
-    'use strict'
-  
-    const forms = document.querySelectorAll('.needs-validation')
-  
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-  
-        form.classList.add('was-validated')
-      }, false)
-    })
-})()
-
 //hiding/showing address section
 function changeVisablityAddress (){
   const Selector = document.querySelector('#VariantSelector');
@@ -34,4 +16,39 @@ function changeVisablityAddress (){
     document.getElementById("City").toggleAttribute("required", false);
     document.getElementById("PLZ").toggleAttribute("required", false);
   }
+}
+
+//Chage Page to display an overview of the Donation
+document.querySelector("form").onsubmit = function displayOverview (){
+  const VariantSelector = document.getElementById("VariantSelector");
+  const ClothTypeSelector = document.getElementById("ClothTypeSelector");
+  const AgeSelector = document.getElementById("AgeSelector");
+  const LocationSelector = document.getElementById("LocationSelector");
+  const Street = document.getElementById("Street");
+  const Housenumber = document.getElementById("Housenumber");
+  const City = document.getElementById("City");
+  const PLZ = document.getElementById("PLZ");
+
+  document.getElementById("genral").classList.add('d-none');
+  document.getElementById("address").classList.add('d-none');
+  document.getElementById("submit").classList.add('d-none');
+
+  document.getElementById("donationOverview").classList.remove('d-none');
+  document.getElementById("SelectedVariant").innerText = VariantSelector.options[VariantSelector.selectedIndex].text;
+  document.getElementById("SelectedClothType").innerText = ClothTypeSelector.options[ClothTypeSelector.selectedIndex].text;
+  document.getElementById("SelectedAge").innerText = AgeSelector.options[AgeSelector.selectedIndex].text;
+  document.getElementById("SelectedLocation").innerText = LocationSelector.options[LocationSelector.selectedIndex].text;
+
+  if(VariantSelector.value==="1"){
+    document.getElementById("enteredStreet").innerText = Street.value;
+    document.getElementById("enteredHuosenumber").innerText = Housenumber.value;
+    document.getElementById("enteredCity").innerText = City.value;
+    document.getElementById("enteredPLZ").innerText = PLZ.value;
+  }else{
+    const addressRows = document.querySelectorAll('.address');
+    for(row of addressRows){
+      row.classList.add('d-none')
+    }
+  }
+  return false;
 }
