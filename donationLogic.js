8ba25047fcc3f15ffbb8a8900539e22d6e0d3b54
atat072 <paulhenke46@gmail.com>
2023-07-14 09:20:24 +0200

    added hinding of address section removed validation for address Section while hidden

--- donationLogic.js
+++ donationLogic.js
@@ -14,4 +14,24 @@
         form.classList.add('was-validated')
       }, false)
     })
-  })()
+})()
+
+//hiding/showing address section
+function changeVisablityAddress (){
+  const Selector = document.querySelector('#VariantSelector');
+  const addressSection = document.querySelector('#address');
+
+  if(Selector.value==="1"){
+    addressSection.classList.remove('d-none');
+    document.getElementById("Street").toggleAttribute("required", true);
+    document.getElementById("Housenumber").toggleAttribute("required", true);
+    document.getElementById("City").toggleAttribute("required", true);
+    document.getElementById("PLZ").toggleAttribute("required", true);
+  }else{
+    addressSection.classList.add('d-none');
+    document.getElementById("Street").toggleAttribute("required", false);
+    document.getElementById("Housenumber").toggleAttribute("required", false);
+    document.getElementById("City").toggleAttribute("required", false);
+    document.getElementById("PLZ").toggleAttribute("required", false);
+  }
+}
